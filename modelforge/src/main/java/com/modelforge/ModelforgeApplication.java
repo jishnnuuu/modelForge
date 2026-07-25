efcd9886d161dfb932e8b005cf8c94f5409cf82f
Jishnu Satwik <jishnusatwikkcp@gmail.com>
2026-07-25 19:23:22 +0530

    initialised spring boot project with maven and java 21

---
+++ modelforge/src/main/java/com/modelforge/ModelforgeApplication.java
@@ -0,0 +1,13 @@
+package com.modelforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelforgeApplication.class, args);
+	}
+
+}
---
+++ modelforge/src/test/java/com/modelforge/ModelforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
