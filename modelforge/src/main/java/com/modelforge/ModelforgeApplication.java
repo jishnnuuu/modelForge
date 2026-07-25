aadb00a9c75f849aff16799e93b6fa6bdc057c74
Jishnu Satwik <jishnusatwikkcp@gmail.com>
2026-07-25 19:31:28 +0530

    feat(api): add hello endpoint using Spring MVC

--- modelforge/src/main/java/com/modelforge/ModelforgeApplication.java
+++ modelforge/src/main/java/com/modelforge/ModelforgeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModelforgeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ModelforgeApplication.class, args);
 	}
