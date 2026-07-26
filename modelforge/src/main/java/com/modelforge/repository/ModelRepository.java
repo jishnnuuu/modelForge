24efac8beddf4332cd01690055d38ea94922297e
Jishnu Satwik <jishnusatwikkcp@gmail.com>
2026-07-26 23:49:34 +0530

    feat(repository): migrate to Spring Data JpaRepository

--- modelforge/src/main/java/com/modelforge/repository/ModelRepository.java
+++ modelforge/src/main/java/com/modelforge/repository/ModelRepository.java
@@ -1,19 +1,9 @@
 package com.modelforge.repository;
 import com.modelforge.model.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
-
 @Repository
-public class ModelRepository {
-    private final List<Model> models = new ArrayList<>();
-
-    public ModelRepository(){
-        models.add(new Model("ResNet50",95.6,2));
-        models.add(new Model("YOLOv8",91.4,4));
-        models.add(new Model("EfficientNet",94.2,3));
-    }
-    public List<Model> findAll(){
-        return models;
-    }
+public interface ModelRepository extends JpaRepository<Model, Long> {
+    
 }
