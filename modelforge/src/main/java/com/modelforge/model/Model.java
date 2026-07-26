4dffd75a36b1f2a578cdd99163a3afe9aa7e1cd0
Jishnu Satwik <jishnusatwikkcp@gmail.com>
2026-07-26 23:49:29 +0530

    feat(model): convert Model to JPA entity

--- modelforge/src/main/java/com/modelforge/model/Model.java
+++ modelforge/src/main/java/com/modelforge/model/Model.java
@@ -1,6 +1,13 @@
 package com.modelforge.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "models")
 public class Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private double accuracy;
     private int version;
@@ -9,6 +16,17 @@ public class Model {
         this.name = name;
         this.accuracy = accuracy;
         this.version = version;
+    }
+
+    public Model() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName(){
