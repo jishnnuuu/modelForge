4709db86942536e62ca8f6fe029b6eb68dc79b3d
Jishnu Satwik <jishnusatwikkcp@gmail.com>
2026-08-15 21:22:05 +0530

    created setters and getters for the model class.

--- modelforge/src/main/java/com/modelforge/model/Model.java
+++ modelforge/src/main/java/com/modelforge/model/Model.java
@@ -44,11 +44,23 @@ public class Model {
         return name;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
+
     public double getAccuracy(){
         return accuracy;
     }
 
+    public void setAccuracy(double accuracy) {
+        this.accuracy = accuracy;
+    }
+
     public int getVersion(){
         return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
     }
 }
